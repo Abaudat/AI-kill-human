0ebbf296917424422aa6b3ca692c63404f6f259a
Adrian Baudat <granbaud@hotmail.com>
2024-06-02 13:48:05 +0200

    Add UI sounds

--- Assets/Scripts/GameView/UI/GlobalSound.cs
+++ Assets/Scripts/GameView/UI/GlobalSound.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 public class GlobalSound : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip sentenceImpossibleClip;
+    public AudioClip sentenceImpossibleClip, uiClick, uiCancel;
 
     public void PlayImpossible()
     {
         audioSource.PlayOneShot(sentenceImpossibleClip);
+    }
+
+    public void PlayUiClick()
+    {
+        audioSource.PlayOneShot(uiClick);
+    }
+
+    public void PlayUiCancel()
+    {
+        audioSource.PlayOneShot(uiCancel);
     }
 }
