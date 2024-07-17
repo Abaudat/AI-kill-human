f6771829f6b8e62f0cf422a29c8eaf9c71c90ffe
abaudat <baudatadrian@gmail.com>
2024-07-17 08:17:50 +0200

    Add animation on drag hover to terminal

--- Assets/SentencePanelDragTarget.cs
+++ Assets/SentencePanelDragTarget.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SentencePanelDragTarget : MonoBehaviour, IDropHandler
+public class SentencePanelDragTarget : MonoBehaviour, IDropHandler, IPointerEnterHandler
 {
+    public Animator animator;
+
     public void OnDrop(PointerEventData eventData)
     {
         WordButton wordButton = eventData.pointerDrag.GetComponent<WordButton>();
         if (wordButton)
         {
             wordButton.PlayWord();
+            animator.SetBool("Hovered", false);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        WordButton wordButton = eventData?.pointerDrag?.GetComponent<WordButton>();
+        if (wordButton)
+        {
+            animator.SetBool("Hovered", true);
         }
     }
 }
