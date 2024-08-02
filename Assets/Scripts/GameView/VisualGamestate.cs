afbe53889c77856646d2b0d0f94c9884181e81cf
abaudat <baudatadrian@gmail.com>
2024-08-02 18:00:09 +0200

    Start UI redesign

--- Assets/Scripts/GameView/VisualGamestate.cs
+++ Assets/Scripts/GameView/VisualGamestate.cs
@@ -9,7 +9,7 @@ public class VisualGamestate : MonoBehaviour
     public GameObject nextStagePanel;
     public Button playSentenceButton;
     public AnnotatedProgressBar stageProgressBar;
-    public Transform aiWordRoot, extraAiWordRoot, killWordRoot, humanWordRoot, extraHumanWordRoot, makeWordRoot, moneyWordRoot;
+    public RectTransform wordRoot;
     public Dialogue aiDiedDialogue;
 
     private CoreInterface coreInterface;
@@ -75,87 +75,24 @@ public class VisualGamestate : MonoBehaviour
 
     private void RegenerateButtons()
     {
-        aiWordRoot.GetComponentsInChildren<WordButton>().ToList().ForEach(x => Destroy(x.gameObject));
-        extraAiWordRoot.GetComponentsInChildren<WordButton>().ToList().ForEach(x => Destroy(x.gameObject));
-        killWordRoot.GetComponentsInChildren<WordButton>().ToList().ForEach(x => Destroy(x.gameObject));
-        humanWordRoot.GetComponentsInChildren<WordButton>().ToList().ForEach(x => Destroy(x.gameObject));
-        extraHumanWordRoot.GetComponentsInChildren<WordButton>().ToList().ForEach(x => Destroy(x.gameObject));
-        makeWordRoot.GetComponentsInChildren<WordButton>().ToList().ForEach(x => Destroy(x.gameObject));
-        moneyWordRoot.GetComponentsInChildren<WordButton>().ToList().ForEach(x => Destroy(x.gameObject));
+        wordRoot.GetComponentsInChildren<WordButton>().ToList().ForEach(x => Destroy(x.gameObject));
 
-        var sortedHumanWords = stageManager.GetWords(coreInterface.coreGamestate)
-            .Where(word => word is HumanWord)
-            .OrderByDescending(word => word);
-        var sortedAiWords = stageManager.GetWords(coreInterface.coreGamestate)
-            .Where(word => word is AiWord)
-            .OrderByDescending(word => word);
-
-        bool shouldSpawnExtraHumansWordButton = false;
-        bool shouldSpawnExtraAisWordButton = false;
-
-        float extraHumansAnimationOffset = 0;
-        float extraAisAnimationOffset = 0;
-
-        foreach ((Word word, int index) in stageManager.GetWords(coreInterface.coreGamestate).Where(word => word is AiWord).Select((value, i) => (value, i)))
+        foreach((Word word, int index) in stageManager.GetWords(coreInterface.coreGamestate).Select((value, i) => (value, i)))
         {
-            float animationOffset = (0 + index * 0.1f) % 1;
+            SpawnWord(index, word);
+        }
+    }
 
-            bool moreThanThreeAis = sortedAiWords.Count() > 3;
-            if ((moreThanThreeAis && index < 2) || (!moreThanThreeAis && index < 3))
-            {
-                Instantiate(wordButtonPrefab, aiWordRoot).GetComponentInChildren<WordButton>().Populate(word, animationOffset);
-            }
-            else
-            {
-                shouldSpawnExtraAisWordButton = true;
-                Instantiate(wordButtonPrefab, extraAiWordRoot).GetComponentInChildren<WordButton>().Populate(word, animationOffset);
-            }
-            extraAisAnimationOffset = (0 + (index + 1) * 0.1f) % 1;
-        }
-        foreach ((Word word, int index) in stageManager.GetWords(coreInterface.coreGamestate).Where(word => word is KillWord).Select((value, i) => (value, i)))
-        {
-            float animationOffset = (0.2f + index * 0.1f) % 1;
-            Instantiate(wordButtonPrefab, killWordRoot).GetComponentInChildren<WordButton>().Populate(word, animationOffset);
-        }
-        foreach ((Word word, int index) in sortedHumanWords.Select((value, i) => (value, i)))
-        {
-            float animationOffset = (0.4f + index * 0.1f) % 1;
+    private void SpawnWord(float index, Word word)
+    {
+        float animationOffset = (index * Mathf.PI) % 1;
+        GameObject wordObject = Instantiate(wordButtonPrefab, wordRoot);
+        wordObject.GetComponentInChildren<WordButton>().Populate(word, animationOffset);
+        wordObject.transform.localPosition = RandomPositionInRect(wordRoot.rect);
+    }
 
-            bool moreThanThreeHumans = sortedHumanWords.Count() > 3;
-            if ((moreThanThreeHumans && index < 2) || (!moreThanThreeHumans && index < 3))
-            {
-                Instantiate(wordButtonPrefab, humanWordRoot).GetComponentInChildren<WordButton>().Populate(word, animationOffset);
-            }
-            else
-            {
-                shouldSpawnExtraHumansWordButton = true;
-                Instantiate(wordButtonPrefab, extraHumanWordRoot).GetComponentInChildren<WordButton>().Populate(word, animationOffset);
-            }
-            extraHumansAnimationOffset = (0 + (index + 1) * 0.1f) % 1;
-        }
-        foreach ((Word word, int index) in stageManager.GetWords(coreInterface.coreGamestate).Where(word => word is MakeWord).Select((value, i) => (value, i)))
-        {
-            float animationOffset = (0.6f + index * 0.1f) % 1;
-            Instantiate(wordButtonPrefab, makeWordRoot).GetComponentInChildren<WordButton>().Populate(word, animationOffset);
-        }
-        foreach ((Word word, int index) in stageManager.GetWords(coreInterface.coreGamestate).Where(word => word is MoneyWord).Select((value, i) => (value, i)))
-        {
-            float animationOffset = (0.8f + index * 0.1f) % 1;
-            Instantiate(wordButtonPrefab, moneyWordRoot).GetComponentInChildren<WordButton>().Populate(word, animationOffset);
-        }
-
-        extraHumansWordButton.SetActive(shouldSpawnExtraHumansWordButton);
-        extraHumansWordButton.transform.SetAsLastSibling();
-        if (shouldSpawnExtraHumansWordButton)
-        {
-            extraHumansWordButton.GetComponentInChildren<Animator>().SetFloat("offset", extraHumansAnimationOffset);
-        }
-
-        extraAisWordButton.SetActive(shouldSpawnExtraAisWordButton);
-        extraAisWordButton.transform.SetAsLastSibling();
-        if (shouldSpawnExtraAisWordButton)
-        {
-            extraAisWordButton.GetComponentInChildren<Animator>().SetFloat("offset", extraHumansAnimationOffset);
-        }
+    private static Vector2 RandomPositionInRect(Rect rect)
+    {
+        return new(Random.Range(rect.xMin, rect.xMax), Random.Range(rect.yMin, rect.yMax));
     }
 }
